0771dcede39ea30bc57d6b72ff17d58676250b79
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-16 19:23:15 +0000

    Item8537: Several fixes - Plugin can now accept that you unselect all values in multi value fields - Plugin is more robust against leading and trailing white space and non-prefect comma lists of values - Documentation improved - Removed the disabled handler codenow know I willw I will not need
    git-svn-id: http://svn.foswiki.org/trunk/MultiTopicSavePlugin@6371 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/MultiTopicSavePlugin.pm
+++ lib/Foswiki/Plugins/MultiTopicSavePlugin.pm
@@ -133,7 +133,7 @@ sub _MULTITOPICSAVESUBMIT {
     my $result = "<input type='hidden' name='redirectweb' value='%WEB%' />" .
                  "<input type='hidden' name='redirecttopic' value='%TOPIC%' />" .
                  "<input type='submit' value='";
-    $result .= $params->{_DEFAULT};
+    $result .= $params->{_DEFAULT} || "Submit All Changes";
     $result .= "' />";
     
     return $result;
@@ -167,10 +167,19 @@ sub _MULTITOPICSAVEINPUT {
     $targettopic = "$targetweb.$targettopic";
     my $multiple = $params->{multiple} || 0;
     my $field = $params->{_DEFAULT};
-    my $value = defined $params->{value} ? $params->{value} : '';
-    my @values = split( / *, */, $value );
 
-    my @options = defined $params->{options} ? split(/ *, */, $params->{options} ) : ();
+    # For single value we leave leading and trailing space. For multiple value
+    # fields it is better for the user that we remove leading and trailing
+    # spaces as they are most like unwanted in these cases.
+    my $value = defined $params->{value} ? $params->{value} : '';
+    my @values = map { s/^\s*(.*?)\s*$/$1/; $_; } split( /\s*,\s*/, $value ); 
+
+    # We assume all leading and trailing spaces are unwanted.
+    my @options = ();  
+    if ( defined $params->{options} ) {
+        @options = map { s/^\s*(.*?)\s*$/$1/; $_; } 
+                   split( /\s*,\s*/, $params->{options} ); 
+    }
 
     if ( $type eq 'text' ) {
         $result = "<input class='foswikiInputField' type='text' ";
@@ -211,6 +220,11 @@ sub _MULTITOPICSAVEINPUT {
         }
         $result .= "</tr>" if ( $counter != $initcounter );
         $result .= "</table>";
+
+        # We need a dummy hidden field to be able to send
+        # none of the checkboxes selected from the browser
+        $result .= "<input type='hidden' name='multitopicsavefield{$targettopic}{$field}' value=''>"
+          if ($type eq 'checkbox');
     }
     elsif ( $type eq 'select' ) {
         $result = "<select "
@@ -236,227 +250,15 @@ sub _MULTITOPICSAVEINPUT {
         }
         
         $result .= "</select>";
+
+        # We need a dummy hidden field to be able to send
+        # none of the checkboxes selected from the browser
+        $result .= "<input type='hidden' name='multitopicsavefield{$targettopic}{$field}' value=''>";
     }
+    # else if the type is unknown we return nothing
 
     return $result;
 }
-
-=BEGIN TML
-
----++ commonTagsHandler($text, $topic, $web, $included, $meta )
-   * =$text= - text to be processed
-   * =$topic= - the name of the topic in the current CGI query
-   * =$web= - the name of the web in the current CGI query
-   * =$included= - Boolean flag indicating whether the handler is
-     invoked on an included topic
-   * =$meta= - meta-data object for the topic MAY BE =undef=
-This handler is called by the code that expands %<nop>MACROS% syntax in
-the topic body and in form fields. It may be called many times while
-a topic is being rendered.
-
-Only plugins that have to parse the entire topic content should implement
-this function. For expanding macros with trivial syntax it is *far* more
-efficient to use =Foswiki::Func::registerTagHandler= (see =initPlugin=).
-
-Internal Foswiki macros, (and any macros declared using
-=Foswiki::Func::registerTagHandler=) are expanded _before_, and then again
-_after_, this function is called to ensure all %<nop>MACROS% are expanded.
-
-*NOTE:* when this handler is called, &lt;verbatim> blocks have been
-removed from the text (though all other blocks such as &lt;pre> and
-&lt;noautolink> are still present).
-
-*NOTE:* meta-data is _not_ embedded in the text passed to this
-handler. Use the =$meta= object.
-
-*Since:* $Foswiki::Plugins::VERSION 2.0
-
-=cut
-
-#sub commonTagsHandler {
-#    my ( $text, $topic, $web, $included, $meta ) = @_;
-#
-#    # If you don't want to be called from nested includes...
-#    #   if( $included ) {
-#    #         # bail out, handler called from an %INCLUDE{}%
-#    #         return;
-#    #   }
-#
-#    # You can work on $text in place by using the special perl
-#    # variable $_[0]. These allow you to operate on $text
-#    # as if it was passed by reference; for example:
-#    # $_[0] =~ s/SpecialString/my alternative/ge;
-#}
-
-=begin TML
-
----++ beforeCommonTagsHandler($text, $topic, $web, $meta )
-   * =$text= - text to be processed
-   * =$topic= - the name of the topic in the current CGI query
-   * =$web= - the name of the web in the current CGI query
-   * =$meta= - meta-data object for the topic MAY BE =undef=
-This handler is called before Foswiki does any expansion of its own
-internal variables. It is designed for use by cache plugins. Note that
-when this handler is called, &lt;verbatim> blocks are still present
-in the text.
-
-*NOTE*: This handler is called once for each call to
-=commonTagsHandler= i.e. it may be called many times during the
-rendering of a topic.
-
-*NOTE:* meta-data is _not_ embedded in the text passed to this
-handler.
-
-*NOTE:* This handler is not separately called on included topics.
-
-=cut
-
-#sub beforeCommonTagsHandler {
-#    my ( $text, $topic, $web, $meta ) = @_;
-#
-#    # You can work on $text in place by using the special perl
-#    # variable $_[0]. These allow you to operate on $text
-#    # as if it was passed by reference; for example:
-#    # $_[0] =~ s/SpecialString/my alternative/ge;
-#}
-
-=begin TML
-
----++ afterCommonTagsHandler($text, $topic, $web, $meta )
-   * =$text= - text to be processed
-   * =$topic= - the name of the topic in the current CGI query
-   * =$web= - the name of the web in the current CGI query
-   * =$meta= - meta-data object for the topic MAY BE =undef=
-This handler is called after Foswiki has completed expansion of %MACROS%.
-It is designed for use by cache plugins. Note that when this handler
-is called, &lt;verbatim> blocks are present in the text.
-
-*NOTE*: This handler is called once for each call to
-=commonTagsHandler= i.e. it may be called many times during the
-rendering of a topic.
-
-*NOTE:* meta-data is _not_ embedded in the text passed to this
-handler.
-
-=cut
-
-#sub afterCommonTagsHandler {
-#    my ( $text, $topic, $web, $meta ) = @_;
-#
-#    # You can work on $text in place by using the special perl
-#    # variable $_[0]. These allow you to operate on $text
-#    # as if it was passed by reference; for example:
-#    # $_[0] =~ s/SpecialString/my alternative/ge;
-#}
-
-=begin TML
-
----++ preRenderingHandler( $text, \%map )
-   * =$text= - text, with the head, verbatim and pre blocks replaced
-     with placeholders
-   * =\%removed= - reference to a hash that maps the placeholders to
-     the removed blocks.
-
-Handler called immediately before Foswiki syntax structures (such as lists) are
-processed, but after all variables have been expanded. Use this handler to
-process special syntax only recognised by your plugin.
-
-Placeholders are text strings constructed using the tag name and a
-sequence number e.g. 'pre1', "verbatim6", "head1" etc. Placeholders are
-inserted into the text inside &lt;!--!marker!--&gt; characters so the
-text will contain &lt;!--!pre1!--&gt; for placeholder pre1.
-
-Each removed block is represented by the block text and the parameters
-passed to the tag (usually empty) e.g. for
-<verbatim>
-<pre class='slobadob'>
-XYZ
-</pre>
-</verbatim>
-the map will contain:
-<pre>
-$removed->{'pre1'}{text}:   XYZ
-$removed->{'pre1'}{params}: class="slobadob"
-</pre>
-Iterating over blocks for a single tag is easy. For example, to prepend a
-line number to every line of every pre block you might use this code:
-<verbatim>
-foreach my $placeholder ( keys %$map ) {
-    if( $placeholder =~ /^pre/i ) {
-        my $n = 1;
-        $map->{$placeholder}{text} =~ s/^/$n++/gem;
-    }
-}
-</verbatim>
-
-__NOTE__: This handler is called once for each rendered block of text i.e.
-it may be called several times during the rendering of a topic.
-
-*NOTE:* meta-data is _not_ embedded in the text passed to this
-handler.
-
-Since Foswiki::Plugins::VERSION = '2.0'
-
-=cut
-
-#sub preRenderingHandler {
-#    my( $text, $pMap ) = @_;
-#
-#    # You can work on $text in place by using the special perl
-#    # variable $_[0]. These allow you to operate on $text
-#    # as if it was passed by reference; for example:
-#    # $_[0] =~ s/SpecialString/my alternative/ge;
-#}
-
-=begin TML
-
----++ postRenderingHandler( $text )
-   * =$text= - the text that has just been rendered. May be modified in place.
-
-*NOTE*: This handler is called once for each rendered block of text i.e. 
-it may be called several times during the rendering of a topic.
-
-*NOTE:* meta-data is _not_ embedded in the text passed to this
-handler.
-
-Since Foswiki::Plugins::VERSION = '2.0'
-
-=cut
-
-#sub postRenderingHandler {
-#    my $text = shift;
-#    # You can work on $text in place by using the special perl
-#    # variable $_[0]. These allow you to operate on $text
-#    # as if it was passed by reference; for example:
-#    # $_[0] =~ s/SpecialString/my alternative/ge;
-#}
-
-
-=begin TML
-
----++ modifyHeaderHandler( \%headers, $query )
-   * =\%headers= - reference to a hash of existing header values
-   * =$query= - reference to CGI query object
-Lets the plugin modify the HTTP headers that will be emitted when a
-page is written to the browser. \%headers= will contain the headers
-proposed by the core, plus any modifications made by other plugins that also
-implement this method that come earlier in the plugins list.
-<verbatim>
-$headers->{expires} = '+1h';
-</verbatim>
-
-Note that this is the HTTP header which is _not_ the same as the HTML
-&lt;HEAD&gt; tag. The contents of the &lt;HEAD&gt; tag may be manipulated
-using the =Foswiki::Func::addToHEAD= method.
-
-*Since:* Foswiki::Plugins::VERSION 2.0
-
-=cut
-
-#sub modifyHeaderHandler {
-#    my ( $headers, $query ) = @_;
-#}
-
 
 =begin TML
 
@@ -503,8 +305,13 @@ sub restMultiTopicSave {
 
             my $topic = $1;
             my $fieldName = $2;
+            my @fieldValues = $query->param($key);
 
-            $parameters{$topic}{$fieldName} = join(", ", $query->param($key) );
+            # Remove empty values, they are most likely dummy values used
+            # to indicate that no value in a multiple checkbox or select
+            # is selected
+            @fieldValues = grep( /.+/, @fieldValues);
+            $parameters{$topic}{$fieldName} = join(", ", @fieldValues );
         }
     }
 
